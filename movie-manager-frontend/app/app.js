1da40707dab49fb45d74df3d8ed4f7344436c60e
Robin Wyss <robin.wyss@mimacom.com>
2014-08-15 09:09:17 +0200

    fixed redirect to default page

--- movie-manager-frontend/app/app.js
+++ movie-manager-frontend/app/app.js
@@ -1,7 +1,9 @@
 (function () {
     angular.module('moviemanager', ['ngRoute', 'layout', 'search'])
-        .config(function($routeProvider) {
-            $routeProvider.otherwise('/search');
+        .config(function ($routeProvider) {
+            $routeProvider.otherwise({
+                redirectTo: '/search'
+            });
         }).run(function () {
             console.log('salut');
         });
