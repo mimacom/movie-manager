bd15ad339dc7ba4adccdb185fa221a221d155c23
Robin Wyss <rowyss@gmail.com>
2014-09-04 12:14:15 +0200

    changed structure of SearchController

--- movie-manager-frontend/app/search/search.controller.js
+++ movie-manager-frontend/app/search/search.controller.js
@@ -3,26 +3,12 @@
         .controller('SearchController', function ($scope, OmdbService, $route, $location) {
             var self = this;
 
-            // handle browser back
-            $scope.$on('$locationChangeSuccess', function (event, path) {
-                var query = $location.search().q;
-                if (query !== self.query) {
-                    $route.reload();
-                }
-            });
-
-            if ($location.search().q) {
-                self.query = $location.search().q;
-                searchMovie();
-            }
-
             self.search = function () {
                 delete self.errors;
                 delete self.movies;
                 $location.search('q', self.query);
                 searchMovie();
             };
-
 
             function searchMovie() {
                 self.loading = true;
@@ -34,6 +20,20 @@
                     self.loading = false;
                 });
             }
+
+            if ($location.search().q) {
+                self.query = $location.search().q;
+                searchMovie();
+            }
+
+
+            // handle browser back
+            $scope.$on('$locationChangeSuccess', function (event, path) {
+                var query = $location.search().q;
+                if (query !== self.query) {
+                    $route.reload();
+                }
+            });
 
         });
 }());
