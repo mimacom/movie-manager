0a59242ac81ab64c22c635542b2a7c9536ee8942
Robin Wyss <rowyss@gmail.com>
2014-09-04 15:51:00 +0200

    added watchlist icon

--- movie-manager-frontend/app/movie-card/movie.card.controller.js
+++ movie-manager-frontend/app/movie-card/movie.card.controller.js
@@ -1,12 +1,14 @@
 (function () {
     angular.module('movie.card')
-        .controller('MovieCardController', function ($routeParams, OmdbService) {
+        .controller('MovieCardController', function ($routeParams, OmdbService, LoginService) {
             var self = this,
                 imdbId = $routeParams.imdbid;
 
+            this.currentUser = LoginService.getCurrentUser();
+
             OmdbService.movieInfo(imdbId).then(function (movie) {
                 self.movie = movie;
-            }, function(error){
+            }, function (error) {
                 self.errors = [error];
             });
 
