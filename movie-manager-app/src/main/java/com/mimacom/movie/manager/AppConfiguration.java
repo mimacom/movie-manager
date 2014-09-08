f03f041385867d4aa9d80fcca404caa595204c3f
Alain Sahli <alain.sahli@mimacom.com>
2014-09-08 18:28:09 +0200

    Removed unused import

--- movie-manager-app/src/main/java/com/mimacom/movie/manager/AppConfiguration.java
+++ movie-manager-app/src/main/java/com/mimacom/movie/manager/AppConfiguration.java
@@ -1,6 +1,5 @@
 package com.mimacom.movie.manager;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
